e6fb3fdc5dcf98e044cdb06370cae4ef7939bd66
Maria <dudahpereira13@gmail.com>
2026-04-07 21:36:59 -0300

    foi?

--- src/main/java/com/mepr/prova/models/ProdutoModel.java
+++ src/main/java/com/mepr/prova/models/ProdutoModel.java
@@ -1,4 +1,17 @@
 package com.mepr.prova.models;
 
+import com.mepr.prova.enums.StatusProduto;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "tb_produtos")
+@Getter
+@Setter
+@Data
 public class ProdutoModel {
 }
